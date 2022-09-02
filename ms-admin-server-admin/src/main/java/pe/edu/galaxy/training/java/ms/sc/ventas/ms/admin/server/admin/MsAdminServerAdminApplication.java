087fcadd4e226115c086329cea65b9e6c7d1afee
mmautinop32 <mk09_19@hotmail.com>
2022-09-02 16:09:00 -0500

    subiendo cambios de release.001.9

---
+++ ms-admin-server-admin/src/main/java/pe/edu/galaxy/training/java/ms/sc/ventas/ms/admin/server/admin/MsAdminServerAdminApplication.java
@@ -0,0 +1,18 @@
+package pe.edu.galaxy.training.java.ms.sc.ventas.ms.admin.server.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsAdminServerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminServerAdminApplication.class, args);
+	}
+
+}
